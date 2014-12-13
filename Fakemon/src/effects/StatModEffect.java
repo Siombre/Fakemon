dc2eb4753b246984af4c6f36953552fa7ae5da93
Fimbria <nathan.bogue@gmail.com>
2014-12-13 16:19:17 -0600

    stat mod effect messages show when they should

--- Fakemon/src/effects/StatModEffect.java
+++ Fakemon/src/effects/StatModEffect.java
@@ -33,11 +33,10 @@ public class StatModEffect extends Effect {
 						message += " fell!";
 					else if(diff == -2)
 						message += " harshly fell!";
-					else if(diff == -12)
-						message += " was minimized!";
 					else if(diff <= -3)
 						message += " severely fell!";
-
+					if(mods[i] == -12)
+					message += " " + Util.possessive(target.getName()) + " " + statNames[i] + " " + " was minimized!";
 				}
 				else if(eff.mods[i] > 0)
 				{
@@ -47,10 +46,10 @@ public class StatModEffect extends Effect {
 						message += " rose!";
 					else if(diff == 2)
 						message += " rose sharply!";
-					else if(diff == 12)
-						message += " was maximized!";
 					else if(diff <= 3)
 						message += " rose drastically!";
+					if(mods[i] == 12)
+					message += " " + Util.possessive(target.getName()) + " " + statNames[i] + " " + " was maximized!";
 				}
 				screen.displayMessage(message);
 			}
