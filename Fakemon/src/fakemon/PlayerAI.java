65c122b7b70b15c1a4c637e9fa7386c9ccf6a2e1
Siombre <themissingones@hotmail.com>
2014-12-13 22:18:47 -0600

    Cleanup

--- Fakemon/src/fakemon/PlayerAI.java
+++ Fakemon/src/fakemon/PlayerAI.java
@@ -1,19 +1,10 @@
 package fakemon;
 
 public class PlayerAI implements BattleAI{
-	/*public BattleAction getAction(BattleScreen battle, int trainer, int pokemon) {
-		MoveMenuDialogBox box = new MoveMenuDialogBox(battle.dialogLoc,battle);
-		
-		box.moveInit(battle.trainers[trainer], battle.acPokemon[trainer][pokemon]);
-		battle.dialog = box;
-		box.go();
-		
-		return box.getMove();
-	}*/
 	
 	@Override
 	public Pokemon getNextPokemon(BattleScreen battle) {
-		
+		//TODO Add in pokemon select screen
 		return null;
 	}
 	@Override
