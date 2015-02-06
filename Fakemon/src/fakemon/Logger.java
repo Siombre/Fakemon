2026759787faa6cf2834530a431a853a1f43b6f2
Siombre <brandbogue@hotmail.com>
2015-02-06 15:22:53 -0600

    Now makes the log folder

--- Fakemon/src/fakemon/Logger.java
+++ Fakemon/src/fakemon/Logger.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 public class Logger {
 	
 	private static FileWriter f;
+	private static boolean enabled = true;
 	public static void log(String s){
+		if(!enabled) return;
 		try {
 			if(f == null)
 			{
@@ -23,12 +25,14 @@ public class Logger {
 	}
 	private static void openLog() {
 		
+		new File(Start.getPath("log")).mkdir();
 		File file = new File(Start.getPath("log"+"/"+System.currentTimeMillis()+".log"));
 		try {
 			file.createNewFile();
 			f = new FileWriter(file);
 		} catch (IOException e) {
 			System.out.println(file.getAbsolutePath());
+			enabled = false;
 			e.printStackTrace();
 		}
 	}
